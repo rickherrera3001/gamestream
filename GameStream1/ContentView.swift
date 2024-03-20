07c83f012d4584c3b34f363d80408fa097e86dac
rickherrera3001 <rihr.3001@gmail.com>
2024-03-20 13:43:39 -0600

    Initial Commit

---
+++ GameStream1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameStream1
+//
+//  Created by Ricardo Developer on 20/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameStream1/GameStream1App.swift
@@ -0,0 +1,17 @@
+//
+//  GameStream1App.swift
+//  GameStream1
+//
+//  Created by Ricardo Developer on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GameStream1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
